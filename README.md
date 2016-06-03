ee04cf98a8312328ad08e9099568cfcd995539e7
Kai Rollmann <kai.rollmann@googlemail.com>
2016-06-03 21:34:10 +0200

    Add the script, a README and a small example

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# latex-color-table
-Color format a table
+# latex-correlations-matrix
+
+Input: CSV-file, output: string to paste into a LaTeX document
+
+## Example
+
+Todo: Example
+
+## Requirements
+
+You need the following packages in your document preamble:
+
+```latex
+\usepackage[dvipsnames]{xcolor}
+\usepackage[underline=true,rounded corners=false]{pgf-umlsd}
+\definecolor{cor-very-weak}{HTML}{BBBBBB}
+\definecolor{cor-weak}{HTML}{EEBD84}
+\definecolor{cor-moderate}{HTML}{F47461}
+%\definecolor{cor-strong}{HTML}{CB2F44}
+\definecolor{cor-strong}{HTML}{F47461}
+\definecolor{cor-very-strong}{HTML}{8B0000}
+
+\newcommand{\hcancel}[1]{%
+    \tikz[baseline=(tocancel.base)]{
+        \node[inner sep=0pt,outer sep=0pt] (tocancel) {#1};
+        \draw[line width=0.5mm, black] (tocancel.south west) -- (tocancel.north east);
+    }%
+}%
+```
+
+The resulting output could be pasted into the following table:
+```
+\begin{table}[ht]
+\tiny
+\setlength{\tabcolsep}{0.5pt}
+\begin{tabular}{ccccc}
+
+< PASTE OUTPUT HERE >
+
+\end{tabular}
+\caption{Cool table}
+\label{tab:metrics-correlations}
+\end{table}
+```
+
+## Usage
+
+python latex-correlations-matrix.py path/to/file --delimiter ';' --strikethrough A--B D--F
+
+### Parameters
+
+- `file` path to the CSV file
+- (optional) `-d, --delimiter` specifies the column delimiter of the CSV file
+- (optional) `-s --strikethrough` specifies pairs of columns by their header names, which should be striked through
+
+## Example
+
+Input CSV file
+```
+M1,M2,M3
+1.0,-0.0036716973315424,0.0625337112778693
+-0.0036716973315424,1.0,0.136044725405646
+0.0625337112778693,0.136044725405646,1.0
+```
+
+The command
+```
+python latex-correlations-matrix.py example/input.csv --delimiter ','
+```
+yields the following output:
+```
+& M1 & M2 & M3\\ \hline
+M1 & \textcolor{cor-very-strong}{1.0} & \textcolor{cor-very-weak}{-0.0} & \textcolor{cor-very-weak}{0.06}\\ \hline
+M2 &  & \textcolor{cor-very-strong}{1.0} & \textcolor{cor-very-weak}{0.14}\\ \hline
+M3 &  &  & \textcolor{cor-very-strong}{1.0}\\ \hline
+```
+
+Pasted into a table frame:
+```
+\begin{table}[ht]
+\begin{tabular}{ccc}
+  & M1 & M2 & M3\\ \hline
+  M1 & \textcolor{cor-very-strong}{1.0} & \textcolor{cor-very-weak}{-0.0} & \textcolor{cor-very-weak}{0.06}\\ \hline
+  M2 &  & \textcolor{cor-very-strong}{1.0} & \textcolor{cor-very-weak}{0.14}\\ \hline
+  M3 &  &  & \textcolor{cor-very-strong}{1.0}\\ \hline
+\end{tabular}
+\caption{Cool table}
+\label{tab:metrics-correlations}
+\end{table}
+```
---
+++ latex-correlations-matrix.py
@@ -0,0 +1,125 @@
+import csv
+import argparse
+
+
+def get_striked_header_pairs(strikethrough):
+    if strikethrough is not None:
+        return [pair.split('--') for pair in strikethrough]
+    else:
+        return []
+
+
+def build_colored_str(v, cancelled):
+    if (v < 0.20):
+        return '\\textcolor{cor-very-weak}{' + str(cancelled) + "}"
+    elif v >= 0.20 and v < 0.40:
+        return '\\textcolor{cor-weak}{' + str(cancelled) + "}"
+    elif v >= 0.40 and v < 0.60:
+        return '\\textcolor{cor-moderate}{' + str(cancelled) + "}"
+    elif v >= 0.60 and v < 0.80:
+        return '\\textcolor{cor-strong}{' + str(cancelled) + "}"
+    elif v >= 0.80 and v <= 1.0:
+        return '\\textcolor{cor-very-strong}{' + str(cancelled) + "}"
+
+
+def build_cancel(v, dim1, dim2, striked_header_pairs):
+    if ([dim1, dim2] in striked_header_pairs) or \
+       ([dim2, dim1] in striked_header_pairs):
+        return "\\hcancel{" + str(v) + "}"
+    return v
+
+
+def build_table_text(csv_header, csv_data, striked_header_pairs):
+    i = 0
+    j = 0
+    table = '& ' + ' & '.join(csv_header) + '\\\\ \\hline \n'
+
+    while i < len(csv_data):
+        j = 0
+        dim1 = csv_header[i]
+        if dim1 == "SS":
+            i += 1
+            continue
+
+        rowstr = dim1
+        while j < len(csv_data[i]):
+            dim2 = csv_header[j]
+            if dim2 == "SS":
+                j += 1
+                continue
+
+            if i > j:
+                rowstr += " & "
+            else:
+                v = float("%.2f" % round(float(csv_data[i][j]), 2))
+                cancelled = build_cancel(
+                    v,
+                    dim1,
+                    dim2,
+                    striked_header_pairs
+                )
+                v = build_colored_str(v, cancelled)
+                rowstr += " & " + v
+            j += 1
+        table += rowstr + '\\\\ \\hline \n'
+        i += 1
+
+    return table
+
+
+# ---------------
+# ARGUMENTS
+# ---------------
+parser = argparse.ArgumentParser(
+    description="This program pretty-prints a CSV correlations matrix for usage in latex",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+
+parser.add_argument(
+    'csv_file',
+    type=str,
+    help='CSV file to load'
+)
+parser.add_argument(
+    '-d',
+    '--delimiter',
+    type=str,
+    default=",",
+    help='Column delimiter',
+)
+parser.add_argument(
+    '-s',
+    '--strikethrough',
+    type=str,
+    nargs='+',
+    help='Column pair to strike through (e.g. A--B, B--F for headers named A to F)',
+)
+
+
+# ---------------
+# MAIN
+# ---------------
+if __name__ != '__main__':
+    raise NotImplementedError("This module should be main")
+
+# Parse arguments
+args = parser.parse_args()
+
+
+# Read CSV file
+header = []
+data = []
+
+with open(args.csv_file, 'rb') as csvfile:
+    all_data = list(csv.reader(csvfile, delimiter=args.delimiter))
+
+    header = all_data[0]
+    data = all_data[1:]
+
+striked_header_pairs = get_striked_header_pairs(args.strikethrough)
+
+
+# Format and print latex commands
+table = build_table_text(header, data, striked_header_pairs)
+
+print table
